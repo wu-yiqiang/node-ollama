a36be3afe5501c1a98e583f7c7c739b414697639
wu-yiqiang <wu_yiqiang@outlook.com>
2025-05-10 11:45:38 +0800

    11

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 import { express } from 'express';
+import { ollama } from '@langchain/community/llms/ollama'
+const app = express()
+const port = 8700
+const model = new ollama({
+  bacUrl: 'http://localhost:11434',
+  model: 'deepseek-r1' 
+})
+
+app.get('/test', async () => {
+  const stream = await model.stream('你是谁')
+
+})
+
+app.listern(port, () => {
+  
+})
+
